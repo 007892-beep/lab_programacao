3c62693e06100875337269d6b97a5e0bea79ff71
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-05-27 17:30:36 -0300

    Excluir triangulo.py

--- Atividades/triangulo.py
+++
@@ -1,5 +0,0 @@
-#este programa calcula a área de um triângulo.
-base = int(input("insira o valor da base: "))
-altura = int(input("insira o valor da altura: "))
-area = (base * altura) / 2
-print(f"o valor da área do triângulo será {area}")
