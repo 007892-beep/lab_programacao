a733b5f61b16cd2bba29bc6686d9d7454f2ed1a3
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-05-28 00:28:23 -0300

    Excluir exercicio01.py

--- Listas /exercicio01.py
+++
@@ -1,7 +0,0 @@
-vetor = []
-for i in range (10):
-    numero = int(input("Digite um vetor: "))
-    vetor.append(numero)
-set_vetor = set(vetor)
-quantidade_diferentes = len(set_vetor)
-print(f"A quantidade de vetores diferentes é: (qunatidade_diiferentes)")    
