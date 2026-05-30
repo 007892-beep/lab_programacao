2c60863244abbc6378da9169cb69254cb7d6867d
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-05-29 22:44:18 -0300

    Update exercicio01.py

--- Atividades/Extra_Sala/exercicio01.py
+++ Atividades/Extra_Sala/exercicio01.py
@@ -1,1 +1,9 @@
-
+while True:
+    num = int(input("Digite um numero (0 - para sair): "))
+    if num == 0:
+        print("Encerramento...")
+        break
+    if num >=10 and num <=50:
+        print("Dado válido!")
+    else:
+        print("Dado inválido!")
