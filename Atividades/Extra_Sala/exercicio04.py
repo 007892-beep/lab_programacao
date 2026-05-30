ddf4c7b79c9e2c83647211d26baa105ced6f45f4
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-05-29 22:49:56 -0300

    Update exercicio04.py

--- Atividades/Extra_Sala/exercicio04.py
+++ Atividades/Extra_Sala/exercicio04.py
@@ -1,1 +1,9 @@
+notas =[]
+for i in range(5):
+    nota = float(input(f"Digite a nota do aluno {i+1}: "))
+    notas.append(nota)
+menor_nota = min(notas)
+notas.remove(menor_nota)
 
+print(f"\nA menor nota ({menor_nota}) foi removida.")
+print(f"Notas restantes na lista: {notas}")
