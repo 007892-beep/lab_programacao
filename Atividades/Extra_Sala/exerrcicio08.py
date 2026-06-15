4b178e09f80b96acc6e085ed02403ae35f5a8236
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-06-15 19:55:12 -0300

    Update exerrcicio08.py

--- Atividades/Extra_Sala/exerrcicio08.py
+++ Atividades/Extra_Sala/exerrcicio08.py
@@ -1,1 +1,17 @@
+vetor = []
+palavra = ""
+frase = input("Digite uma frase curta separada por espaços")
 
+for carac in frase:
+    if carac == " ":
+        if palavra != "":
+            vetor.append(palavra)
+            palavra = ""
+    else:
+        palavra += carac
+
+if palavra != "":
+    vetor.append(palavra)
+
+print("\nResultado do split manual:")
+print(vetor)
