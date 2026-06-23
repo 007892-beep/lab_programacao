434b1be3a5caf3fa22a32d72994e5f34fcdfe465
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-06-23 19:52:29 -0300

    Create menu.py

---
+++ subrpogramação /menu.py
@@ -0,0 +1,15 @@
+def menu():
+    print('************')
+    print('1 - Somar')
+    print('2 - Multiplicar')
+    print('3 - Dividir')
+    print('*********************')
+
+
+
+menu()
+opcao = int(input('Digite a opção desejada: '))
+
+
+menu()
+menu()
