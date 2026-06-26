9f9cad1ca029fecf00acc3cca2b3ce5fc35b8ca7
Luiza de Paula Vieira Cau <007892@libertas.edu.br>
2026-06-25 21:03:11 -0300

    Update exercicio01.py

--- subrpogramação /exercicio01.py
+++ subrpogramação /exercicio01.py
@@ -1,1 +1,11 @@
-
+def verificar_status(media):
+    if media >=6:
+        return "Aprovado"
+    elif media >=4:
+        return "verificação suplementar"
+    else:
+        return "Reprovado"
+    
+nota = float(input("Digite a média do aluno: "))
+status = verificar_status(nota)
+print(f"Status do aluno: (status)")
